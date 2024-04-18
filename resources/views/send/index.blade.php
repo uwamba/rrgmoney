8080ab662215014270ae5d4ca9dfd81e21ad636e
Dieudonne <uwambadodo@gmail.com>
2024-04-18 18:30:40 +0200

    add frw dispaly in send and change number format to currency format

--- resources/views/send/index.blade.php
+++ resources/views/send/index.blade.php
@@ -63,7 +63,7 @@
                                                                 <td>{{ $sent->created_on }}</td>
                                                                 <td>{{ $sent->class }}</td>
                                                                 <td>{{ $sent->amount_local_currency }}</td>
-                                                                <td>{{ $sent->amount_rw_currency }}</td>
+                                                                <td>{{ $sent->amount_rw }}</td>
                                                                 <td>{{ $sent->amount_foregn_currency }}</td>
                                                                 <td>{{ $sent->first_name." ".$sent->last_name }}</td>
                                                                 <td>{{ $sent->mobile_number }}</td>
