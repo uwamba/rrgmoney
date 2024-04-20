aa90a8db05b70792e2f7074f113e116f74613aab
Dieudonne <uwambadodo@gmail.com>
2024-04-20 14:19:10 +0200

    add frw dispaly in send and change number format to currency format

--- resources/views/email/receiverNotification.blade.php
+++ resources/views/email/receiverNotification.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>rrgmoney.com</title>
 </head>
+
 <body>
-   <p>Hello {{$mailData['receiverName']}},</p>
-   <p> You  have received a money transfer from {{$mailData['senderName']}}.</p>
-   <p>Please note that the transaction is pending approval and will be processed shortly</p>
-   <p>Best regards, </p>
-   <p>Thank you, </p>
-   <p>RRGMONEY</p>";
+    <p>Hello {{ $mailData['receiverName'] }},</p>
+    <p> We are pleased to inform you that you have received a money transfer from {{ $mailData['senderName'] }}.</p>
+    <p>Rest assured, the transaction will be processed shortly.</p>
+    <p>Best regards, </p>
+    <p>RRGMONEY</p>";
+
 </body>
+
 </html>
--- resources/views/email/senderNotification.blade.php
+++ resources/views/email/senderNotification.blade.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>rrgmoney.com</title>
 </head>
+
 <body>
-   <p>Hello {{$mailData['senderName']}},</p>
-   <p> This is to inform you that you have successfully initiated a money transfer to {{$mailData['receiverName']}}.".</p>
-   <p>Please note that the transaction is pending for approval and will be processed shortly.</p>
-   <p>Thank you for using rrgmoney for your money transfers.</p>
-   <p>If you have any questions or need assistance, feel free to reach out. </p>
-   <p>Best regards, </p>
-   <p>Thank you, </p>
-   <p>RRGMONEY</p>";
+    <p>Hello {{ $mailData['senderName'] }},</p>
+    <p> We'd like to inform you that your money transfer to {{ $mailData['receiverName'] }} has been successfully
+        initiated.</p>
+
+    <p>Rest assured, the transaction will be processed shortly.</p>
+
+    <p>Thank you for choosing RRGMONEY.</p>
+
+    <p>Best regards,</p>
+
+    <p>RRGMONEY </p>
+
 </body>
+
 </html>
