fa7601069d88d192d9708f7551bffec38af593db
Dieudonne <uwambadodo@gmail.com>
2024-04-25 10:09:18 +0200

    remove delete before any other stock request done

--- resources/views/home_admin.blade.php
+++ resources/views/home_admin.blade.php
@@ -40,22 +40,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                My Balance </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$ <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                               Users </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{$stock_balance}}</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fa-regular fa-laptop-binary"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>}}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fa-regular fa-laptop-binary"></i>
