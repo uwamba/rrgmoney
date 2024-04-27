b85283f6cbf0717854cfdf1c66878a121c49c95e
Dieudonne <uwambadodo@gmail.com>
2024-04-27 09:51:11 +0200

    add testing project for firebase phone verfication

--- resources/views/auth/authenticationTest.blade.php
+++ resources/views/auth/authenticationTest.blade.php
@@ -34,17 +34,11 @@
         </div>
     @endsection
 
+    <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase.js"></script>
 
-    <script type="module">
-        // Import the functions you need from the SDKs you need
-        import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-        import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
-        // TODO: Add SDKs for Firebase products that you want to use
-        // https://firebase.google.com/docs/web/setup#available-libraries
-      
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        const firebaseConfig = {
+    <script>
+       
+        var firebaseConfig = {
           apiKey: "AIzaSyAA-OYxLCAuQzgqUXP1fvvnWo6zMDoadjg",
           authDomain: "test-rrgmoney.firebaseapp.com",
           projectId: "test-rrgmoney",
