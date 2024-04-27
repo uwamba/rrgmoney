0bdec693a2c382de7346a9aa54f6bdb44ea9ff98
Dieudonne <uwambadodo@gmail.com>
2024-04-27 09:37:22 +0200

    add testing project for firebase phone verfication

--- resources/views/auth/authenticationTest.blade.php
+++ resources/views/auth/authenticationTest.blade.php
@@ -35,19 +35,30 @@
     @endsection
 
 
-    <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase.js"></script>
-   
-    <script>
-    const firebaseConfig = {
-        apiKey: "AIzaSyAA-OYxLCAuQzgqUXP1fvvnWo6zMDoadjg",
-        authDomain: "test-rrgmoney.firebaseapp.com",
-        projectId: "test-rrgmoney",
-        storageBucket: "test-rrgmoney.appspot.com",
-        messagingSenderId: "559118850458",
-        appId: "1:559118850458:web:0a6c2dfe0adacdbc5b0223",
-        measurementId: "G-YBQE9H4WTF"
-      };
-    </script>
+    <script type="module">
+        // Import the functions you need from the SDKs you need
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+        import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+      
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+          apiKey: "AIzaSyAA-OYxLCAuQzgqUXP1fvvnWo6zMDoadjg",
+          authDomain: "test-rrgmoney.firebaseapp.com",
+          projectId: "test-rrgmoney",
+          storageBucket: "test-rrgmoney.appspot.com",
+          messagingSenderId: "559118850458",
+          appId: "1:559118850458:web:0a6c2dfe0adacdbc5b0223",
+          measurementId: "G-YBQE9H4WTF"
+        };
+      
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const analytics = getAnalytics(app);
+      </script>
+      
 
     
 
