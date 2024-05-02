207a21d59199e84bc321940ee95db43c2b678014
Dieudonne <uwambadodo@gmail.com>
2024-05-02 12:03:45 +0200

    update permission seed by adding all permissions

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,12 +15,28 @@ class RoleSeeder extends Seeder
     public function run()
     {
         Role::create([
-            'name' => 'Admin',
+            'name' => 'SuperAdmin',
             'guard_name' => 'web',
         ]);
 
         Role::create([
             'name' => 'User',
+            'guard_name' => 'web',
+        ]);
+        Role::create([
+            'name' => 'Customer',
+            'guard_name' => 'web',
+        ]);
+        Role::create([
+            'name' => 'Agent',
+            'guard_name' => 'web',
+        ]);
+        Role::create([
+            'name' => 'Finance',
+            'guard_name' => 'web',
+        ]);
+        Role::create([
+            'name' => 'Admin',
             'guard_name' => 'web',
         ]);
     }
