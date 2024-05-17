38279c533285e1a9e5dd8de2b99c0c9234ea894e
Dieudonne <uwambadodo@gmail.com>
2024-05-17 12:06:39 +0200

    fix stock account

--- resources/views/home_admin.blade.php
+++ resources/views/home_admin.blade.php
@@ -41,7 +41,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$key}}</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$value['account_name']}}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fa-regular fa-laptop-binary">{{$value}}</i>
