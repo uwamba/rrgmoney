6cbf0abc067ef78211e91fa405a933801141ffab
Dieudonne <uwambadodo@gmail.com>
2024-05-18 11:27:07 +0200

    fix stock account

--- resources/views/agent/calculator.blade.php
+++ resources/views/agent/calculator.blade.php
@@ -100,7 +100,7 @@
                                                 @foreach ($currencies as $currency)
                                                     <option value="{{ $currency->currency_name }}"
                                                         data-rate="{{ $currency->currency_buying_rate }}">
-                                                        {{ $currency->currency_name . ' ' . $currency_buying_rate->currency_selling_rate }}
+                                                        {{ $currency->currency_name . ' ' . $currency->currency_buying_rate }}
                                                     </option>
                                                 @endforeach
 
