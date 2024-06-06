924ebe7055dc37bffd37bc99d3c826e28f02e715
Dieudonne <uwambadodo@gmail.com>
2024-06-06 14:26:42 +0200

    Send controller store refactoring

--- app/Http/Requests/CreateTransferRequest.php
+++ app/Http/Requests/CreateTransferRequest.php
@@ -16,11 +16,7 @@ class CreateTransferRequest extends FormRequest
 
     public function authorize()
     {
-            $this->middleware('auth');
-            $this->middleware('permission:send-list|send-create|send-edit|send-delete', ['only' => ['index']]);
-            $this->middleware('permission:send-create', ['only' => ['create','store']]);
-            $this->middleware('permission:send-edit', ['only' => ['edit','update']]);
-            $this->middleware('permission:send-delete', ['only' => ['destroy']]);
+
 
     }
 
