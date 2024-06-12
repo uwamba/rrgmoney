04d1cd264e3e83760cf70e89dc194c1f70112936
Dieudonne <uwambadodo@gmail.com>
2024-06-12 14:21:45 +0200

    add option to choose account on transfer approval

--- resources/views/send/confirm-approval-modal.blade.php
+++ resources/views/send/confirm-approval-modal.blade.php
@@ -8,11 +8,9 @@
                     <span aria-hidden="true">×</span>
                 </button>
             </div>
-              <div class="modal-body">
-
-                      <table class="table table-striped">
-                       <form method="POST" action="{{ route('send.approve') }}">
-                            @csrf
+            <div class="modal-body">
+                <form method="POST" action="{{ route('send.approve') }}">
+                        @csrf
                         <input type="hidden" id="id" name="id"/>
                         <input type="hidden" id="account_name" name="account" />
                         <input type="hidden" id="amount_rw_currency" name="amount_rw_currency" />
@@ -25,12 +23,12 @@
                         <input type="hidden" id="receiver_id" name="receiver_id" />
                         <input type="hidden" id="status" name="status" value="Approved"/>
 
-                        <p id="test"><p/>
-                               <div class="input-group " style="margin-bottom:2px;">
-                                 <span style="color:red;">*</span>Receiver Currency</label>
-                              </div>
 
-                              <select class="form-control form-control-user @error('account') is-invalid @enderror" name="account_name" id="account_name">
+                        <div class="input-group " style="margin-bottom:2px;">
+                          <span style="color:red;">*</span>Receiver Currency</label>
+                        </div>
+                        <div class="input-group " style="margin-bottom:2px;">
+                          <select class="form-control form-control-user @error('account') is-invalid @enderror" name="account_name" id="account_name">
                                  <option selected disabled>Select Account</option>
 
                                  @foreach ($accounts as $account)
@@ -39,22 +37,15 @@
                                  </option>
                                  @endforeach
 
-                                </select>
-                               @error('account')
-                              <span class="text-danger">{{ $message }}</span>
-                             @enderror
-                             <div class="modal-footer">
-                                <button class="btn btn-secondary" onclick="closeConfirmModal()" type="button" data-dismiss="modal">Cancel</button>
-                                 <button type="submit" class="btn btn-success ">Confirm</button>
+                            </select>
+                       <div/>
+                       <div class="input-group " style="margin-bottom:2px;">
+                           <button class="btn btn-secondary" onclick="closeConfirmModal()" type="button" data-dismiss="modal">Cancel</button>
+                           <button type="submit" class="btn btn-success ">Confirm</button>
+                       </div>
 
-                            </div>
-                            </form>
-
-                            </table>
-
-
-
-              </div>
+                </form>
+            </div>
 
         </div>
     </div>
