a1858e786688f777e4d38564327d6acf84efc305
Dieudonne <uwambadodo@gmail.com>
2024-06-12 15:29:05 +0200

    add reject button

--- resources/views/send/reject-modal.blade.php
+++ resources/views/send/reject-modal.blade.php
@@ -11,11 +11,16 @@
             <form id="user-delete-form" method="POST" action="{{ route('send.reject', ['transfer' => $sent->id]) }}">
                 @csrf
                 @method('POST')
-                       <input type="hidden" name="id" value="{{$sent->id}}"/>
-                       <input type="hidden" name="agent_id" value="{{$sent->user_id}}"/>
-                       <input type="hidden" name="sender_id" value="{{$sent->sender_id}}"/>
-                       <input type="hidden" name="receiver_id" value="{{$sent->receiver_id}}"/>
-                       <input type="hidden" name="status" value="Approved"/>
+                <input type="input" id="id" name="id"/>
+                <input type="input" id="amount_rw_currency" name="amount_rw_currency" />
+                <input type="input" id="amount_local_currency" name="amount_local_currency" />
+                <input type="input" id="currency" name="currency" />
+                <input type="input" id="sender_currency" name="sender_currency"   />
+                <input type="input" id="amount_foregn_currency" name="amount_foregn_currency" />
+                <input type="input" id="agent_id" name="agent_id" />
+                <input type="input" id="sender_id" name="sender_id" />
+                <input type="input" id="receiver_id" name="receiver_id" />
+                <input type="input" id="status" name="status" value="Rejected"/>
                 <div class="modal-body">
                 <p>Select "Reject" below if you want to reject this Transfer!.</p><div class="mb-3">
                     <label for="FormControlTextarea" class="form-label">Reason to reject</label>
