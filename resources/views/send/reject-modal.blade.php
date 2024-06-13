cf4a3be4c9d6654dd9a0f6a29e1c7705d8bb09cc
Dieudonne <uwambadodo@gmail.com>
2024-06-13 10:04:35 +0200

    add reject button and fix approval

--- resources/views/send/reject-modal.blade.php
+++ resources/views/send/reject-modal.blade.php
@@ -11,16 +11,16 @@
             <form id="user-delete-form" method="POST" action="{{ route('send.reject', ['transfer' => $sent->id]) }}">
                 @csrf
                 @method('POST')
-                <input type="input" id="id" name="id"/>
-                <input type="input" id="amount_rw_currency" name="amount_rw_currency" />
-                <input type="input" id="amount_local_currency" name="amount_local_currency" />
-                <input type="input" id="currency" name="currency" />
-                <input type="input" id="sender_currency" name="sender_currency"   />
-                <input type="input" id="amount_foregn_currency" name="amount_foregn_currency" />
-                <input type="input" id="agent_id" name="agent_id" />
-                <input type="input" id="sender_id" name="sender_id" />
-                <input type="input" id="receiver_id" name="receiver_id" />
-                <input type="input" id="status" name="status" value="Rejected"/>
+                <input type="hidden" id="id" name="id"/>
+                <input type="hidden" id="amount_rw_currency" name="amount_rw_currency" />
+                <input type="hidden" id="amount_local_currency" name="amount_local_currency" />
+                <input type="hidden" id="currency" name="currency" />
+                <input type="hidden" id="sender_currency" name="sender_currency"   />
+                <input type="hidden" id="amount_foregn_currency" name="amount_foregn_currency" />
+                <input type="hidden" id="agent_id" name="agent_id" />
+                <input type="hidden" id="sender_id" name="sender_id" />
+                <input type="hidden" id="receiver_id" name="receiver_id" />
+                <input type="hidden" id="status" name="status" value="Rejected"/>
                 <div class="modal-body">
                 <p>Select "Reject" below if you want to reject this Transfer!.</p><div class="mb-3">
                     <label for="FormControlTextarea" class="form-label">Reason to reject</label>
