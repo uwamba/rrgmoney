c36e03034a3cb5234193b699de2683d8d1443814
Dieudonne <uwambadodo@gmail.com>
2024-07-01 09:21:27 +0200

    add discount on tranfer

--- resources/views/agent/send/transferNext.blade.php
+++ resources/views/agent/send/transferNext.blade.php
@@ -182,6 +182,16 @@
                                                 class="btn btn-info p-2 mb-2">Calculate</a>
                                         </div>
                                     </div>
+                                    <div class="input-group " style="margin-bottom:2px;">
+                                        <Label>Discount in %</Label>
+                                    </div>
+                                    <div class="input-group ">
+                                        <input type="text" class="p-2 mb-2 text-black form-control"
+                                            placeholder="Enter Discount in %" aria-label="Search"
+                                            name="discount" id="discount"
+                                            aria-describedby="basic-addon2" data-type="discount">
+
+                                    </div>
                                     <ul class="list-group list-group-flush">
                                         <li class="list-group-item" id="charges"></li>
                                         <li class="list-group-item" id="total_amount_local"></li>
