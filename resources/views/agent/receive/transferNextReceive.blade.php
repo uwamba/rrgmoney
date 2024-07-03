2e8a325cc0efe81449e6182612433c4790802158
Dieudonne <uwambadodo@gmail.com>
2024-07-03 11:09:16 +0200

    add discount on tranfer

--- resources/views/agent/receive/transferNextReceive.blade.php
+++ resources/views/agent/receive/transferNextReceive.blade.php
@@ -158,6 +158,16 @@
                                         @enderror
                                     </div>
                                     <div class="input-group " style="margin-bottom:2px;">
+                                        <Label>Discount in %</Label>
+                                    </div>
+                                    <div class="input-group ">
+                                        <input type="text" class="p-2 mb-2 text-black form-control"
+                                            placeholder="Enter Discount in %" aria-label="Search"
+                                            name="discount" id="discount"
+                                            aria-describedby="basic-addon2" data-type="discount">
+
+                                    </div>
+                                    <div class="input-group " style="margin-bottom:2px;">
                                         <Label>Amount to be Sent</Label>
                                     </div>
 
