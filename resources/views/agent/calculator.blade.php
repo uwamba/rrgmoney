2871adf89d1db96c36ca52018ffa95c7200467d5
Dieudonne <uwambadodo@gmail.com>
2024-07-03 11:43:55 +0200

    add discount on tranfer

--- resources/views/agent/calculator.blade.php
+++ resources/views/agent/calculator.blade.php
@@ -75,16 +75,7 @@
 
                                         </select>
                                     </div>
-                                    <div class="input-group " style="margin-bottom:2px;">
-                                        <Label>Discount in %</Label>
-                                    </div>
-                                    <div class="input-group ">
-                                        <input type="text" class="p-2 mb-2 text-black form-control"
-                                            placeholder="Enter Discount in %" aria-label="Search"
-                                            name="discount" id="discount"
-                                            aria-describedby="basic-addon2" data-type="discount">
 
-                                    </div>
                                         <div class="input-group-append">
                                             <a href="javascript:void(0)" id="amount_sent_btn"
                                                 class="btn btn-info p-2 mb-2 ">Calculate</a>
@@ -121,6 +112,16 @@
                                             <a href="javascript:void(0)" id="amount_receive_btn"
                                                 class="btn btn-info p-2 mb-2">Calculate</a>
                                         </div>
+                                    </div>
+                                    <div class="input-group " style="margin-bottom:2px;">
+                                        <Label>Discount in %</Label>
+                                    </div>
+                                    <div class="input-group ">
+                                        <input type="text" class="p-2 mb-2 text-black form-control"
+                                            placeholder="Enter Discount in %" aria-label="Search"
+                                            name="discount" id="discount"
+                                            aria-describedby="basic-addon2" data-type="discount">
+
                                     </div>
                                     <ul class="list-group list-group-flush">
                                         <li class="list-group-item" id="charges"></li>
