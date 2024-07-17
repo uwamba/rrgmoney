5b6096e958813ce8968db5cd384fd5f56d527a4a
Dieudonne <uwambadodo@gmail.com>
2024-07-17 15:29:24 +0200

    working on search

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -36,6 +36,7 @@
             <form
             method="POST" action="{{route('users.userSearch')}}"
             class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            @csrf
             <div class="input-group">
                 <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
                     name="query"
