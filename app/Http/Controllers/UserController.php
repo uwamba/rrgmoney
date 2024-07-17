71a3c32c8d25cd734fa8d7889671488af4ad7905
Dieudonne <uwambadodo@gmail.com>
2024-07-17 15:18:24 +0200

    working on search

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,23 +41,8 @@ class UserController extends Controller
     public function index()
     {
 
-       // $users = User::with('roles')->paginate(10);
-       // return view('users.index', ['users' => $users]);
-
-
-
-        $q = $request->input('query');
-
-        $query = User::query()
-            ->latest()
-            ->select(['id', 'first_name', 'last_name', 'email', 'country', 'created_at'])
-            ->where(function (Builder $subQuery) use ($q) {
-                $subQuery->where('first_name', 'like', '%'.$q.'%')
-                    ->orWhere('last_name', 'like', '%'.$q.'%')
-                    ->orWhere('email', 'like', '%'.$q.'%');
-            });
-
-            return view('users.index', ['users' => $query]);
+        $users = User::with('roles')->paginate(10);
+        return view('users.index', ['users' => $users]);
     }
     public function customerList()
         {
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,6 +6,18 @@
     <div class="container-fluid">
 
         <!-- Page Heading -->
+        <form
+        class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+        <div class="input-group">
+            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                aria-label="Search" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                    <i class="fas fa-search fa-sm"></i>
+                </button>
+            </div>
+        </div>
+    </form>
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Users</h1>
             <div class="row">
