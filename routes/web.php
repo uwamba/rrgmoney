7d71bee6b2a7a9e42ce618896a65213ce0eef3b0
Dieudonne <uwambadodo@gmail.com>
2024-07-18 14:30:08 +0200

    working on search

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,7 @@ Route::middleware('auth')->prefix('StockAccount')->name('StockAccount.')->group(
     Route::get('/index', [StockAccountController::class, 'index'])->name('index');
     Route::get('/create', [StockAccountController::class, 'create'])->name('create');
     Route::post('/store', [StockAccountController::class, 'store'])->name('store');
+    Route::post('/stockAccountSearch', [StockAccountController::class, 'stockAccountSearch'])->name('stockAccountSearch');
     Route::post('/setDefault', [StockAccountController::class, 'setDefault'])->name('setDefault');
     Route::get('/edit', [StockAccountController::class, 'edit'])->name('edit');
     Route::post('/delete', [StockAccountController::class, 'delete'])->name('delete');
