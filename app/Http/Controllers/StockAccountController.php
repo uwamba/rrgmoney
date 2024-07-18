aa5125b3e811bcbdb827d9686964d79dcee4a3ad
Dieudonne <uwambadodo@gmail.com>
2024-07-18 14:36:10 +0200

    working on search

--- app/Http/Controllers/StockAccountController.php
+++ app/Http/Controllers/StockAccountController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Builder;
+use App\Models\StockAccount;
 
 class StockAccountController extends Controller
 {
