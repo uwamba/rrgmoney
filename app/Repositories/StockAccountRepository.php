5ba8a9c3b9b304fa1160624076fd4bc316b1b0f3
Dieudonne <uwambadodo@gmail.com>
2024-07-18 14:45:21 +0200

    working on search

--- app/Repositories/StockAccountRepository.php
+++ app/Repositories/StockAccountRepository.php
@@ -6,6 +6,7 @@ use App\Models\StockAccount;
 use App\Interfaces\StockAccountInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Builder;
 
 
 class StockAccountRepository implements StockAccountInterface{
