f3548aecf09730851df71de93017534ed0819008
Dieudonne <uwambadodo@gmail.com>
2024-07-23 12:06:37 +0200

    working on transfer reprt

--- resources/views/send/report.blade.php
+++ resources/views/send/report.blade.php
@@ -2,30 +2,7 @@
 <html>
 <head>
     <title>Laravel Livewire Example - ItSolutionStuff.com</title>
-    <style >
-        [wire\:loading], [wire\:loading\.delay], [wire\:loading\.inline-block], [wire\:loading\.inline], [wire\:loading\.block], [wire\:loading\.flex], [wire\:loading\.table], [wire\:loading\.grid], [wire\:loading\.inline-flex] {
-            display: none;
-        }
-
-        [wire\:loading\.delay\.shortest], [wire\:loading\.delay\.shorter], [wire\:loading\.delay\.short], [wire\:loading\.delay\.long], [wire\:loading\.delay\.longer], [wire\:loading\.delay\.longest] {
-            display:none;
-        }
-
-        [wire\:offline] {
-            display: none;
-        }
-
-        [wire\:dirty]:not(textarea):not(input):not(select) {
-            display: none;
-        }
-
-        input:-webkit-autofill, select:-webkit-autofill, textarea:-webkit-autofill {
-            animation-duration: 50000s;
-            animation-name: livewireautofill;
-        }
-
-        @keyframes livewireautofill { from {} }
-    </style>
+    @livewireStyles
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.2/tailwind.min.css" integrity="sha512-l7qZAq1JcXdHei6h2z8h8sMe3NbMrmowhOl+QkP3UhifPpCW2MC4M0i26Y8wYpbz1xD9t61MLT9L1N773dzlOA==" crossorigin="anonymous" />
 </head>
 <body>
@@ -49,6 +26,6 @@
 
 </body>
 
-<script src="/public/vendor/livewire/livewire.js?id=90730a3b0e7144480175" data-turbo-eval="false" data-turbolinks-eval="false" ></script>
+@livewireScripts
 
 </html>
