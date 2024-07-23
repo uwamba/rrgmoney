26423212f40f3b65a9bb62e296153e6b7fc5951c
Dieudonne <uwambadodo@gmail.com>
2024-07-23 11:58:23 +0200

    working on transfer reprt

--- resources/views/send/report.blade.php
+++ resources/views/send/report.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Laravel Livewire Example - ItSolutionStuff.com</title>
-    @livewireStyles()
+    @livewireStyles
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.2/tailwind.min.css" integrity="sha512-l7qZAq1JcXdHei6h2z8h8sMe3NbMrmowhOl+QkP3UhifPpCW2MC4M0i26Y8wYpbz1xD9t61MLT9L1N773dzlOA==" crossorigin="anonymous" />
 </head>
 <body>
@@ -26,6 +26,6 @@
 
 </body>
 
-@livewireScripts()
+<script src="/public/vendor/livewire/livewire.js?id=90730a3b0e7144480175" data-turbo-eval="false" data-turbolinks-eval="false" ></script>
 
 </html>
