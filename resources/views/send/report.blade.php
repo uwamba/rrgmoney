d42160c62770392762da31a8cb738dabf6aa49f2
Dieudonne <uwambadodo@gmail.com>
2024-07-30 13:35:43 +0200

    working on transfer reprt

--- resources/views/send/report.blade.php
+++ resources/views/send/report.blade.php
@@ -4,15 +4,7 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <livewire:transfer-chart/>
-                    </div>
-                </div>
-            </div>
-        </div>
+
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
